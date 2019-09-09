f3e93d80dcd1c690cd01c0e4ebd1bc38525f3cad
Andy Wang <andy.wang@blockchaintech.net.au>
2019-09-09 22:15:21 +1000

    Create User model

---
+++ src/main/java/com/example/copsboot/user/User.java
@@ -0,0 +1,47 @@
+package com.example.copsboot.user;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "copsboot_user")
+public class User {
+    @Id
+    private UUID id;
+    private String email;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Set<UserRole> roles;
+
+    protected User() {
+
+    }
+
+    public User(UUID id, String email, String password, Set<UserRole> roles) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+}
